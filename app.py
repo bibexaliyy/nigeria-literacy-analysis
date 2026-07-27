287bdf8d411e66d32b3c3e20c8f77e74e5c89d74
bibexaliyy <hisah075@gmail.com>
2026-07-27 12:37:49 +0100

    requirements spelling corrected

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.set_page_config(page_title="Nigeria Literacy analysis", layout="wide")
